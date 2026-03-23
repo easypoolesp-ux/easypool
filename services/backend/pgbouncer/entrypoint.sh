9549ca97cd7cb00e562d7eafdd94db573252dba1
lalit <easypoolesp@gmail.com>
2026-03-24 00:44:23 +0530

    feat: fix YAML indentation and refine pgbouncer auth for sidecar

--- services/backend/pgbouncer/entrypoint.sh
+++ services/backend/pgbouncer/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 # Replace the ${DB_PASSWORD} placeholder with the actual environment variable
+sed -i "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /etc/pgbouncer/pgbouncer.ini
 sed -i "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /etc/pgbouncer/userlist.txt
 
 # Start PgBouncer
